47c5d788efa27bc4e32eab979797ef5752eda26f
mintedd <xxxxiheartyouxxxxx@gmail.com>
2022-11-02 14:30:07 -0400

    added jquery and bootstrap link

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
-fetch('http://api.openweathermap.org/geo/1.0/direct?q=&appid={899885609b42fb1c311343820acf2670}'),{
-    //q = parameters  {city name} {state code}  {country code}
-    //method:GET?
+// fetch('http://api.openweathermap.org/geo/1.0/direct?q=&appid={899885609b42fb1c311343820acf2670}'),{
+//     //q = parameters  {city name} {state code}  {country code}
+//     //method:GET?
+// }
+// .then()
+
+var today = moment();
+$('#current-day').text(today.format("MMM Do YY"))
+
+function currentDay(){
+    
+   
 }
-.then()
