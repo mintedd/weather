0a567fd8befcf582663a17f20b2843ed95164b5f
mintedd <xxxxiheartyouxxxxx@gmail.com>
2022-11-02 14:17:31 -0400

    added pseudocode and base elements

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+fetch('http://api.openweathermap.org/geo/1.0/direct?q=&appid={899885609b42fb1c311343820acf2670}'),{
+    //q = parameters  {city name} {state code}  {country code}
+    //method:GET?
+}
+.then()
