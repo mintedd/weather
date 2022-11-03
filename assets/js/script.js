41e3c0e966ddb77806747286fc591079da3e425f
mintedd <xxxxiheartyouxxxxx@gmail.com>
2022-11-03 14:17:27 -0400

    added bootstrap and googlefonts

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,20 @@
 //     //method:GET?
 // }
 // .then()
+//first api is for weather
+//second api is for turn long and lat into city 
+//how to get the icons for weather
+//stores this city into local storage 
 
-var today = moment();
-$('#current-day').text(today.format("MMM Do YY"))
+// declarations
+var searchBtnEl = $(".btn")
 
 function currentDay(){
-    
-   
+   var today = moment().format("MMM Do YY");
+    $('#current-day').text(today) 
 }
+
+// $(".btn").on('click', function())
+searchBtnEl.addEventListener('click', function(){
+    alert("its working");
+})
